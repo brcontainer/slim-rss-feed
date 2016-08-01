a9895f805c4a8ea471d52efd4000d856921dbcdb
Guilherme Nascimento <brcontainer@yahoo.com.br>
2016-07-31 22:30:01 -0300

    Improvements
    Added detect content-type
    News icons
    Replace pageAction by browserAction

--- js/background.js
+++ js/background.js
@@ -11,11 +11,27 @@
 
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (request.type === "update" && request.items && request.items.length) {
-            chrome.pageAction.show(sender.tab.id);
+            //chrome.pageAction.show(sender.tab.id);
+
+            chrome.browserAction.setIcon({
+                "tabId": sender.tab.id,
+                "path": "images/active.png"
+            });
 
             allFeeds["tab_" + sender.tab.id] = request.items;
         } else if (request.type === "get" && request.id && allFeeds["tab_" + request.id]) {
             sendResponse(allFeeds["tab_" + request.id]);
         }
     });
+
+    function handleHeaders(details)
+    {
+        console.log(details.responseHeaders, details.url);
+    }
+
+    chrome.webRequest.onHeadersReceived.addListener(handleHeaders, {
+        urls: ['https://*/*', 'http://*/*'],
+        types: ['main_frame']
+    },
+    ['responseHeaders', 'blocking']);
 })();
